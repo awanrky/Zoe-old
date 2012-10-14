cae1c0227ce0d387c050bbad51f4419e79f5840b
unknown <mark.ott@live.com>
2012-10-13 23:52:55 -0500

    started doing some knockout

---
+++ Node/public/index.js
@@ -0,0 +1,28 @@
+(function() {
+	$(document).ready(function() {
+		//$('#person').load('Mark');
+
+		function IndexViewModel() {
+			var self = this;
+			this.firstName = ko.observable("");
+			this.middleName = ko.observable("");
+			this.lastName = ko.observable("");
+			this.fullName = ko.computed(function() {
+				return this.firstName() + ' ' + this.middleName() + ' ' + this.lastName();
+			}, this);
+
+			this.receivedData = ko.observableArray([{d:''}]);
+
+			$.getJSON("/Mark", function(data) {
+				self.receivedData.push({d: JSON.stringify(data)});
+				var person = data[0];
+				self.firstName(person.name.first);
+				self.middleName(person.name.middle);
+				self.lastName(person.name.last);
+			});
+		}
+
+		ko.applyBindings(new IndexViewModel());
+
+	});
+}());
